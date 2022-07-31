89beb414fe0ab98adf8a3f539081fc47593eef57
Felipe Rocha <lipe.ferreira2002@gmail.com>
2022-07-30 21:21:55 -0300

    Segunda etapa

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,162 @@
-import express from 'express'
+import express from "express";
 
-const app = express()
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
-app.get('/', (request, response) => {
-  return response.status(200).json({ alive: true })
-})
+function validateEmail(email: string): boolean {
+  var validate = /\S+@\S+\.\S+/;
+  return validate.test(email);
+}
 
-export { app }
+function validateFields(value: any): boolean {
+  if (value == null) {
+    return false;
+  }
+  return value;
+}
+
+function validateDevices(devices: devices[]) {
+  let type = true;
+  let condition = true;
+
+  if (devices == undefined) {
+    return false;
+  }
+
+  for (var i = 0; i < devices.length; i++) {
+    type = typeof devices[i].type === "string";
+    condition = typeof devices[i].condition === "string";
+    if (!type || !condition) {
+      return false;
+    }
+  }
+  return type && condition;
+}
+
+function filterDevices(devices: devices[]) {
+  const data = devices.map((item: devices) => {
+    return {
+      type: item.type,
+      condition: item.condition,
+    };
+  });
+  return data;
+}
+
+interface devices {
+  type: string;
+  condition: string;
+}
+
+const data: {
+  name: string;
+  email?: string;
+  phone: number;
+  zip: number;
+  city: string;
+  state: string;
+  streetAddress: string;
+  number: number;
+  complement?: string;
+  neighborhood: string;
+  deviceCount: number;
+  devices: devices[];
+}[] = [];
+
+// Criar rota POST /donation que receberá os dados em um único post
+app.post("/donation", (request, response) => {
+  const {
+    name,
+    email,
+    phone,
+    zip,
+    city,
+    state,
+    streetAddress,
+    number,
+    complement,
+    neighborhood,
+    deviceCount,
+    devices,
+  } = request.body;
+
+  // Validar se o endereço de email é válido
+  if (email) {
+    if (!validateEmail(email)) {
+      return response
+        .status(400)
+        .json({ error: true, errorMessage: "O email não é válido!" });
+    }
+  }
+
+  // Se algum campo faltar retornar status 400 com {error: true, requiredFields: [$field1, $field2, ...], errorMessage: "Todos os campos obrigatórios devem ser informados"}
+  if (
+    !validateFields(name) ||
+    !validateFields(phone) ||
+    !validateFields(zip) ||
+    !validateFields(city) ||
+    !validateFields(state) ||
+    !validateFields(streetAddress) ||
+    !validateFields(number) ||
+    !validateFields(neighborhood) ||
+    !validateFields(deviceCount)
+  ) {
+    return response.status(400).json({
+      error: true,
+      requiredFiels: [
+        "name",
+        "phone",
+        "zip",
+        "city",
+        "state",
+        "streetAddress",
+        "number",
+        "neighborhood",
+        "deviceCount",
+        "devices",
+      ],
+      errorMessage: "Todos os campos obrigatórios devem ser informados",
+    });
+  }
+
+  // Validar se os devices estão chegando com os types corretos ou se não estão chegando
+  if (!validateDevices(devices)) {
+    return response.status(400).json({
+      error: true,
+      errorMessage: "O campo de 'devices' não é válido ou não foi informado!",
+    });
+  }
+
+  // Se a quantidade de itens no array devices for diferente de deviceCount retornar status 400 com {error:true, errorMessage: "A quantidade de equipamentos ({$deviceCount}) não está de acordo com as informações de equipamentos enviados ({$sentDevices})"
+  if (deviceCount != devices.length) {
+    return response.status(400).json({
+      error: true,
+      errorMessage: `A quantidade de equipamentos ${deviceCount} não está de acordo com as informações de equipamentos enviados ${devices.length}`,
+    });
+  }
+
+  data.push({
+    name,
+    email,
+    phone,
+    zip,
+    city,
+    state,
+    streetAddress,
+    number,
+    complement,
+    neighborhood,
+    deviceCount,
+    devices: filterDevices(devices),
+  });
+
+  // Se todos os dados estiverem ok, consideraremos que houve sucesso e então retornaremos status 200 com json {success:true}
+  return response.status(200).json({ success: true });
+});
+
+app.get("/donation", (request, response) => {
+  return response.json(data);
+});
+
+export { app };
--- src/test.spec.ts
+++ src/test.spec.ts
@@ -1,11 +1,170 @@
-import request from 'supertest'
-import { app } from './app'
+import request from "supertest";
+import { app } from "./app";
 
-describe('Test', () => {
-  it('Testing route get', async () => {
-    const response = await request(app).get('/')
+describe("All tests", () => {
+  // Criar teste para enviar campos faltando, e deverá ter sucesso ao confirmar que a API irá recusar com status 400 e errorMessage
+  it("Should not be able to send data without a obligatory parameter(name)", async () => {
+    const response = await request(app)
+      .post("/donation")
+      .send({
+        email: "test@test.com",
+        phone: 77988001122,
+        zip: 45100000,
+        city: "city test",
+        state: "state test",
+        streetAddress: "street test",
+        number: 12,
+        complement: "complement test",
+        neighborhood: "test",
+        deviceCount: 1,
+        devices: [
+          {
+            type: "type test",
+            condition: "condition test",
+          },
+        ],
+      });
+    expect(response.status).toBe(400);
+    expect(response.body.error).toBe(true);
+    expect(response.body.errorMessage).toEqual(
+      "Todos os campos obrigatórios devem ser informados"
+    );
+  });
 
-    expect(response.body).toEqual({ alive: true })
-    expect(response.status).toBe(200)
-  })
-})
+  // Informar um email inválido, e confirmar que a API recusa com mensagem específica pra isso
+  it("Should not be able to send email invalid", async () => {
+    const response = await request(app)
+      .post("/donation")
+      .send({
+        name: "test",
+        email: "test.com",
+        phone: 77988001122,
+        zip: 45100000,
+        city: "city test",
+        state: "state test",
+        streetAddress: "street test",
+        number: 12,
+        complement: "complement test",
+        neighborhood: "test",
+        deviceCount: 1,
+        devices: [
+          {
+            type: "type test",
+            condition: "condition test",
+          },
+        ],
+      });
+    expect(response.status).toBe(400);
+    expect(response.body.error).toBe(true);
+    expect(response.body.errorMessage).toEqual("O email não é válido!");
+  });
+
+  // Criar teste para enviar dados pessoais completos, mas não enviar devices
+  it("Should not be able to send data without devices", async () => {
+    const response = await request(app).post("/donation").send({
+      name: "test",
+      email: "test@test.com",
+      phone: 77988001122,
+      zip: 45100000,
+      city: "city test",
+      state: "state test",
+      streetAddress: "street test",
+      number: 12,
+      complement: "complement test",
+      neighborhood: "test",
+      deviceCount: 1,
+    });
+    expect(response.status).toBe(400);
+    expect(response.body.error).toBe(true);
+    expect(response.body.errorMessage).toEqual(
+      "O campo de 'devices' não é válido ou não foi informado!"
+    );
+  });
+
+  // Criar teste que envie dados completos, e deviceCount for diferente da quantidade de itens enviados em devices, deverá retornar uma falha
+  it("Should not be able to send data with deviceCount different from devices length", async () => {
+    const data = {
+      name: "test",
+      email: "test@test.com",
+      phone: 77988001122,
+      zip: 45100000,
+      city: "city test",
+      state: "state test",
+      streetAddress: "street test",
+      number: 12,
+      complement: "complement test",
+      neighborhood: "test",
+      deviceCount: 2,
+      devices: [
+        {
+          type: "type test",
+          condition: "condition test",
+        },
+      ],
+    };
+
+    const response = await request(app).post("/donation").send(data);
+
+    expect(response.status).toBe(400);
+    expect(response.body.error).toBe(true);
+    expect(response.body.errorMessage).toEqual(
+      `A quantidade de equipamentos ${data.deviceCount} não está de acordo com as informações de equipamentos enviados ${data.devices.length}`
+    );
+  });
+
+  // Criar teste que envie um type inválido
+  it("Should not be able to send data with invalid type", async () => {
+    const response = await request(app)
+      .post("/donation")
+      .send({
+        name: "test",
+        email: "test@teste.com",
+        phone: 77988001122,
+        zip: 45100000,
+        city: "city test",
+        state: "state test",
+        streetAddress: "street test",
+        number: 12,
+        complement: "complement test",
+        neighborhood: "test",
+        deviceCount: 1,
+        devices: [
+          {
+            test: "type test",
+          },
+        ],
+      });
+    expect(response.status).toBe(400);
+    expect(response.body.error).toBe(true);
+    expect(response.body.errorMessage).toEqual(
+      "O campo de 'devices' não é válido ou não foi informado!"
+    );
+  });
+
+  // Criar teste que envie dados e devices corretamente, que enfim retornará 200 e sucess
+  it("Should be able to send data with devices", async () => {
+    const response = await request(app)
+      .post("/donation")
+      .send({
+        name: "test",
+        email: "test@teste.com",
+        phone: 77988001122,
+        zip: 45100000,
+        city: "city test",
+        state: "state test",
+        streetAddress: "street test",
+        number: 12,
+        complement: "complement test",
+        neighborhood: "test",
+        deviceCount: 1,
+        devices: [
+          {
+            type: "type test",
+            condition: "condition test",
+          },
+        ],
+      });
+    expect(response.status).toBe(200);
+    expect(response.body.success).toBe(true);
+  });
+});
